4bdf7c502b9c8e035894de3633f1f34697a3f480
Leandro Corrêa <scleandro@gmail.com>
2021-01-05 16:09:06 -0200

    Meu primeiro commit dentro do netbeans

--- public_html/scripts/formscript.js
+++ public_html/scripts/formscript.js
@@ -1,23 +1,20 @@
-
+/*
 function sendEmail() { 
-      Email.send({ 
+        let nome = document.getElementsById("txtnome").value;
+        let email = document.getElementById("email").value;
+        let telefone = document.getElementsById("telefone").value;
+        let mensagem = document.getElementsById("txtmensagem").value;
+        
+        Email.send({ 
         Host: "smtp.outlook.com", 
         Username: "leandros_correa@outlook.com", 
         Password: "!testeMail@", 
         To: 'leandros_correa@outlook.com', 
         From: "leandros_correa@outlook.com", 
         Subject: "Contato para GB Grande Colorado", 
-        Body: "Contato para GB Grande Colorado!!",
-        
-        var nome = document.getElementsById("txtnome").value;
-        var email = document.getElementById("email").value;
-        var telefone = document.getElementsById("telefone").value;
-        var mensagem = document.getElementsById("txtmensagem").value;
-    
-        document.get(nome + email + telefone + mensagem);
-    
+        Body: "nome + email + telefone + mensagem\n",
+        "Contato para GB Grande Colorado!!"
       })
-      
         .then(function (message) { 
           alert("mail sent successfully") 
         }); 
